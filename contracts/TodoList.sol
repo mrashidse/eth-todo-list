154bfbdf0fa96c954ce577c2ff2807e947597b79
Rashid <m.rashid.se@gmail.com>
2020-01-02 20:36:21 +0500

    2 list Tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -8,6 +8,20 @@ contract TodoList {
 	uint public taskCount = 0;
 
 	constructor() public {
-
+		createTask("Checkout my 1st Demo Task");
 	}
+
+	struct Task {
+		uint id;
+		string content;
+		bool completed;
+	}
+
+	mapping (uint => Task) public tasks;
+	
+	function createTask (string memory _content) public {
+		taskCount++;
+		tasks[taskCount] = Task(taskCount, _content, false);
+	}
+	
 }
